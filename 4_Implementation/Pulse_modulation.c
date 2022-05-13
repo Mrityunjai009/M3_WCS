dcd0224a9b353fad4b4955049e8cfbff4c3405d6
mrityunjai singh <mrityunjaisingh550kl@gmail.com>
2022-05-13 13:43:43 +0530

    updating

---
+++ 4_Implementation/Pulse_modulation.c
@@ -0,0 +1,11 @@
+#include "Pulse_modulation.C"
+
+#include <avr/io.h>
+
+void PWM_Initialize()
+{
+    SET_BIT(DDRD,PD6);
+    TCNT0 =0x00;  // Permition to Turning &COUNTER
+    TCCR0A |= ((1<<WGM00)|(1<<WGM01)|(1<<COM0A1)); //
+    TCCR0B |= ((1<<CS00)|((1<<CS01))); //clock and WGM02
+}
---
+++ 4_Implementation/Turning_on.c
@@ -0,0 +1,14 @@
+#include <avr/io.h>
+#include "Turning_on.h"
+
+void TIMER_Initialize()
+{
+
+    TCNT0 =0x00; // ENABLING TURNING
+    TCCR0A=0x00;
+    TCCR0B |= ((1<<CS00)|((1<<CS01)));
+
+}
+
+
+
